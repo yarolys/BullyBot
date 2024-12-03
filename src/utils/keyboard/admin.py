2a945de95e6a3a50d123da99d5dea91e64395bc9
yarolys <lysov.yaroslav.r@mail.ru>
2024-12-03 22:15:17 +0800

    feat(keyboards): enhance admin panel with additional options
    - Updated `admin_panel_kb` to reorder existing buttons for improved UX.
    - Added `add_audio_kb` to provide options for adding static or dynamic sounds.
    - Added `del_audio_kb` to provide options for deleting static or dynamic sounds.

--- src/utils/keyboard/admin.py
+++ src/utils/keyboard/admin.py
@@ -2,9 +2,25 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 admin_panel_kb = ReplyKeyboardMarkup(
     keyboard=[
+        [KeyboardButton(text='Посмотреть приветственное сообщение')],
+        [KeyboardButton(text='Изменить приветственное сообщение')],
         [KeyboardButton(text='Добавить новый звук')],
         [KeyboardButton(text='Удалить звук')],
-        [KeyboardButton(text='Посмотреть приветственное сообщение')],
-        [KeyboardButton(text='Изменить приветственное сообщение')]
+    ],
+    resize_keyboard=True)
+
+add_audio_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Добавить статический звук')],
+        [KeyboardButton(text='Добавить динамический звук')],
+        [KeyboardButton(text='Вернуться в меню')]
+    ],
+    resize_keyboard=True)
+
+del_audio_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Удалить статический звук')],
+        [KeyboardButton(text='Удалить динамический звук')],
+        [KeyboardButton(text='Вернуться в меню')]
     ],
     resize_keyboard=True)
