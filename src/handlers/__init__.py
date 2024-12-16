75e88234221b6b326e2505958ab367c71c18c571
yarolys <lysov.yaroslav.r@mail.ru>
2024-12-16 23:56:46 +0800

    feat: add routers for admin panel and audio features
    - Added routers for handling admin panel actions such as adding new buttons, checking and deleting buttons, and managing welcome messages.
    - Integrated voice router for audio functionality.

---
+++ src/handlers/__init__.py
@@ -0,0 +1,10 @@
+from .start import router as start_router
+from .join2group import router as join2group_router
+
+from .panel_for_admin.admin_panel import router as admin_panel_router
+from .panel_for_admin.add_new_button import router as add_new_button_router
+from .panel_for_admin.check_add_buttons import router as check_add_buttons_router
+from .panel_for_admin.delete_button import router as delete_button_router
+from .panel_for_admin.welcome_message import router as welcome_message_router
+
+from .audio.voice import router as voice_router
