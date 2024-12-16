51c64d25aa3486c21f8ae0bb4be54f58c79a02ed
yarolys <lysov.yaroslav.r@mail.ru>
2024-12-16 23:40:27 +0800

    refactor: update dispatcher to include all routers
    - Reorganized imports for better readability and structure.
    - Added the following routers to `Dispatcher`:
      - `join2group_router`
      - `admin_panel_router`
      - `add_new_button_router`
      - `check_add_buttons_router`
      - `delete_button_router`
      - `welcome_message_router`
    - Ensured all routers are registered and integrated with the bot.

--- run.py
+++ run.py
@@ -2,17 +2,32 @@ import asyncio
 
 from aiogram import Dispatcher
 from src.config import bot, logger
-from src.handlers.start import router as start_router
-from src.handlers.audio.voice import router as voice_router
-from src.handlers.panel_for_admin.admin_panel import router as panel_for_admin_router
+from src.handlers import (
+    start_router,
+    join2group_router,
+    voice_router,
+    admin_panel_router,
+    add_new_button_router,
+    check_add_buttons_router,
+    delete_button_router,
+    welcome_message_router
+
+
+)
 
 
 async def main():
     dp = Dispatcher()
     dp.include_routers(
         start_router,
+        join2group_router,
         voice_router,
-        panel_for_admin_router,
+        admin_panel_router,
+        add_new_button_router,
+        check_add_buttons_router,
+        delete_button_router,
+        welcome_message_router
+
     )
     r = await bot.get_me()
     logger.info(f"Бот запущен: https://t/me/{r.username}")
