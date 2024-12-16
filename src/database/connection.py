baa7a6e1ea19c615fed89cc64117f83abca23553
yarolys <lysov.yaroslav.r@mail.ru>
2024-12-16 23:44:13 +0800

    refactor: update database connection to use DATABASE_URL from config
    - Replaced the direct `os.getenv` call for `DATABASE_URL` with an import from `src.config` to centralize configuration management.
    - Ensured the use of a consistent and modular approach to accessing environment variables.

--- src/database/connection.py
+++ src/database/connection.py
@@ -4,10 +4,9 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from dotenv import load_dotenv
+from src.config import DATABASE_URL
 
 load_dotenv()
-
-DATABASE_URL = os.getenv("SQLALCHEMY_URL")
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) # noqa
