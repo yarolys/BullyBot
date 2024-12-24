a412e85dca40c77e545cec4f28439d73363821bc
yarolys <lysov.yaroslav.r@mail.ru>
2024-12-24 20:34:10 +0800

    feat(admin): enhance welcome message and dynamic button configuration
    - Added support for setting the number of dynamic buttons in admin panel.
    - Replaced hardcoded admin ID checks with `AdminRoleFilter`.
    - Refactored logic for editing and viewing welcome messages.
    - Improved user instructions for message formatting and button configuration.
    
    BREAKING CHANGE: Admin checks now rely on `AdminRoleFilter`, which may require additional configuration for proper functioning.

--- src/handlers/panel_for_admin/welcome_message.py
+++ src/handlers/panel_for_admin/welcome_message.py
@@ -4,6 +4,7 @@ from aiogram.types import Message
 
 from src.config import BOT_ADMIN_ID
 from src.states.admin import FSM_admin_panel
+from src.utils.filter import AdminRoleFilter
 from src.utils.keyboard.join2group import welcome_keyboard
 from src.utils.welcome_message import configure_welcome_message
 
@@ -11,12 +12,8 @@ from src.database.models import DbSettings
 
 router = Router()
 
-@router.message(F.text == 'Посмотреть приветственное сообщение')
+@router.message(F.text == 'Посмотреть приветственное сообщение', AdminRoleFilter())
 async def get_welcome_message(message: Message, state: FSMContext):
-    if message.from_user.id != BOT_ADMIN_ID:
-        await message.answer('Только для администратора')
-        await message.delete()
-        return
     welcome_message = await configure_welcome_message(
         user_full_name=message.from_user.full_name,
         username=message.from_user.username,
@@ -31,12 +28,8 @@ async def get_welcome_message(message: Message, state: FSMContext):
     await state.clear()
 
 
-@router.message(F.text == 'Изменить приветственное сообщение')
+@router.message(F.text == 'Изменить приветственное сообщение', AdminRoleFilter())
 async def edit_welcome_message(message: Message, state: FSMContext):
-    if message.from_user.id != BOT_ADMIN_ID:
-        await message.answer('Только для администратора')
-        await message.delete()
-        return
     await message.answer(
         'Отправь мне текст для сообщения, можешь использовать встроенное форматирование от ТГ\n\n'
         'Чтобы я понял, куда поставить имя пользователя и username используй:\n\n'
@@ -52,3 +45,21 @@ async def get_message(message: Message, state: FSMContext):
     await message.answer('Текст для приветственного сообщения установлен')
     await state.clear()
         
+
+@router.message(F.text == 'Установить количество динамических кнопок',  AdminRoleFilter())
+async def send_amount_of_buttons(message: Message, state: FSMContext):
+    await message.answer('Пришлите мне количество динамических кнопок (числом)')
+    await state.set_state(FSM_admin_panel.get_amount_of_dynamic_buttons)
+
+
+@router.message(FSM_admin_panel.get_amount_of_dynamic_buttons,  AdminRoleFilter())
+async def get_amount_of_dynamic_buttons(message: Message, state: FSMContext):
+    try:
+        amount = int(message.text)
+    except ValueError:
+        await message.answer('Неверный формат')
+        await message.answer('Пришлите мне количество динамических кнопок (числом)')
+        return
+    await DbSettings.set_settings(dynamic_button_count=amount)
+    await message.answer(f'Установлено <b>{amount}</b> динамических кнопок для сообщения')
+    await state.clear()
